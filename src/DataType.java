a6cf753c95885a60bb9307112f7da4d03a33da71
carolin randala <carolinrandala@gmail.com>
2022-09-15 10:55:53 +0300

    Added datatypes

---
+++ src/DataType.java
@@ -0,0 +1,45 @@
+/**
+ * Example of Pimitive datatype
+ *
+ * @author carolinrandala
+ */
+ public class DataType {
+    //Numbers
+    int a = 1;
+    byte byteNumber = 120;
+    long longNumber = 545454681351L;
+    short shortNumber = -32000;
+    float decimalNumber = 2.45f;
+    double doubleNumber = 3.456d;
+
+    //Character
+    char alpha = 'F';
+
+    //True/False
+    boolean hadLunch = true; // had lunch?
+    boolean isHoliday = false;
+
+    /*Multi-line
+    comment
+     */
+
+    int i; // 0
+
+    float x; // 0.00f
+
+    char animal; // ''
+
+    boolean isClassTomorrow; // false
+
+
+
+
+
+
+
+
+
+
+
+
+}
