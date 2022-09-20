e243797a645599890f72b0bc893df30d3ada2bf2
carolin randala <carolinrandala@gmail.com>
2022-09-20 16:15:25 +0300

    Homework added

---
+++ src/Homework2.java
@@ -0,0 +1,14 @@
+public class Homework2 {
+    public static void main(String[] args) {
+        String x = "car";
+        String y = "cake";
+        String change;
+
+        change = x;
+        x = y;
+        y = change;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+}
