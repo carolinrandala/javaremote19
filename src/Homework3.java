5abb03e16f55e2130d656f988c4950d86b472527
carolin randala <carolinrandala@gmail.com>
2022-09-21 17:09:11 +0300

    Homework3 added

---
+++ src/Homework3.java
@@ -0,0 +1,12 @@
+/**
+ * Homework3
+ * @author carolinrandala
+ */
+public class Homework3 {
+    public static void main(String[] args) {
+        //Solution 2
+        //Homework: one line solution for FIZZBUZZ
+
+        int j = 15;
+
+        System.out.println(j % 3 == 0 && j % 5 == 0 ? "fizzbuzz " : ((j % 3 == 0) ? "fizz " : ((j % 5 == 0) ? "buzz " : j + " ")));
