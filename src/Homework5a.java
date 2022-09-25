8ae63d1be749b38bb06d9fddd76faff3b6c514c1
carolin randala <carolinrandala@gmail.com>
2022-09-25 18:23:41 +0300

    explanations added

--- src/Homework5a.java
+++ src/Homework5a.java
@@ -4,13 +4,13 @@
  */
 public class Homework5a {
     public static void main(String[] args) {
-
+        //Initialize array
         int a[]={1, 7, 3, 10, 9};
         System.out.println("Odd Numbers:");
 
+        //Searching and printing out odd numbers
         for(int i=0;i<a.length;i++){
             if(a[i]%2!=0){
-
                 System.out.println(a[i]);
             }
         }
--- src/Homework5b.java
+++ src/Homework5b.java
@@ -4,13 +4,18 @@
  */
 public class Homework5b {
     public static void main(String[] args) {
+
+        //Initialize array
         int a[]={1, 7, 3, 10, 9};
         System.out.println("Odd Numbers:");
+
+        //Searching for odd numbers
         for(int i=0;i<a.length;i++){
             if(a[i]%2!=0){
                 System.out.println(a[i]);
             }
         }
+        //Searching for even numbers
         System.out.println("Even Numbers:");
         for(int i=0;i<a.length;i++){
             if(a[i]%2==0){
