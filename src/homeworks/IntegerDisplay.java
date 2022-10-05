2fa68ac2feb24102ed6472aff3930c8e324f01fa
carolin randala <carolinrandala@gmail.com>
2022-10-05 18:57:16 +0300

    homework done

--- src/homeworks/IntegerDisplay.java
+++ src/homeworks/IntegerDisplay.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class IntegerDisplay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter a number:");
         int number = scanner.nextInt();
 
