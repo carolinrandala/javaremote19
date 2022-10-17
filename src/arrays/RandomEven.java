a0ff4ea0c73b47ed98e5f5fae850d6ede7349f98
carolin randala <carolinrandala@gmail.com>
2022-10-17 19:52:25 +0200

    doing ex

--- src/arrays/RandomEven.java
+++ src/arrays/RandomEven.java
@@ -1,6 +1,10 @@
 package arrays;
 
+
+import java.util.Arrays;
+
 import java.util.Random;
+
 
 /**
  * Write a program that does the following(the code should be written in separate methods):
@@ -23,6 +27,9 @@ public class RandomEven {
         for(int i = 0; i < arrayLimit; i++) {
             array[i] = getRandomNumber(min, max);
         }
+        displayArray(array);
+        displayEven(array);
+        displayOdd(array);
     }
 
     private static int getRandomNumber(int min, int max) {
@@ -30,9 +37,33 @@ public class RandomEven {
         return random.nextInt(max -  min) + min;
     }
 
-    //Display array
+    private static void displayArray(int[] inputArray) {
+        System.out.println(Arrays.toString(inputArray));
+    }
 
-    //To print only odd numbers of array
+    /*display array
+    print only odd numbers of array
+    print only even numbers of array
+    call these three matters from the main?
+    */
+    private static void displayEven(int[] inputArray) {
+        System.out.println("Even Numbers:");
 
-    //to print even numbers of array
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] % 2 == 0) {
+                System.out.println(inputArray[i]);
+            }
+        }
+    }
+
+    private static void displayOdd(int[] inputArray){
+        System.out.println("Odd Numbers:");
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] % 2 != 0) {
+                System.out.println(inputArray[i]);
+            }
+
+        }
+    }
 }
+
