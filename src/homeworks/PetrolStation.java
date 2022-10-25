0b06bf8d1a3a64431e38442eeb1cb12d9f4f7b93
carolin randala <carolinrandala@gmail.com>
2022-10-25 20:06:03 +0300

    homework made

--- src/homeworks/PetrolStation.java
+++ src/homeworks/PetrolStation.java
@@ -1,6 +1,7 @@
 package homeworks;
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -12,32 +13,90 @@ import java.util.Scanner;
  * The user paid too little – should be asked for the rest.
  */
 public class PetrolStation {
+    private static final double THRESHOLD_AMOUNT = 5;
+    private static final double PETROL_PRICE_PER_LITRE = 2.199;
+    private static final String CURRENCY = "EUR";
+    static double totalQuantity = 0;
+    static double totalAmount = 0;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        final double thresholdPetrol = 5;
-        final double pricePerLitre = 2.199;
+        System.out.println("WELCOME TO PETROL STATION");
+        System.out.println("=========================");
+        System.out.println("Today's petrol price: " + PETROL_PRICE_PER_LITRE + CURRENCY);
+        System.out.println();
 
-        System.out.println("Petrol refilling");
+        System.out.println("Do you want to refill the tank? (true/false)");
 
-        //Within the while loop ask user if you should continue or finish.
-        System.out.println("Would you like to continue or finish?");
-        boolean isContinue = scanner.nextBoolean();
-        boolean finish = scanner.nextBoolean();
-        double totalLitres = 0;
-        double totalAmount = 0;
-        isContinue = true;
-        finish = true;
+        while (isContinue()) {
+            totalQuantity += THRESHOLD_AMOUNT;
+            totalAmount = totalQuantity * PETROL_PRICE_PER_LITRE;
 
-        while (isContinue) {
-            totalLitres += thresholdPetrol;
-            totalAmount = totalLitres * pricePerLitre;
-            System.out.println("Total litres filled:" + totalLitres);
-            if(finish) {
-                System.out.println("Total amount:" + totalAmount + " €");
+            displayCart();
+            System.out.println("Do you want to fill more? (true/false)");
+        }
+
+        System.out.println("Thanks for fueling!");
+        displayCart();
+
+        //PAYMENT
+        if (totalAmount > 0) {
+            System.out.println("Do you wish to pay now? (true/false)");
+
+            if(isContinue()) {
+                boolean isMoneyPaid = false;
+
+                while(!isMoneyPaid) {
+                    double money = getMoney();
+                    System.out.println("Money paid: "  + money + CURRENCY);
+                    String message = "Thanks for the payment! Visit us again!";
+
+                    if(money == totalAmount) {
+                        System.out.println(message);
+                        isMoneyPaid = true;
+                    } else if(money > totalAmount) {
+                        double balance = money - totalAmount;
+                        System.out.println("Balance returned: " + balance + CURRENCY);
+                        System.out.println(message);
+                        isMoneyPaid = true;
+                    } else {
+                        System.out.println("The amount is paid is lesser than the total amount! Please pay the correct amount.");
+                    }
+                }
+            } else {
+                System.out.println("Invoice will be sent to you! Thank you!");
             }
         }
+
+    }
+
+    private static boolean isContinue() {
+        Scanner scanner = new Scanner(System.in);
+        String errorMessage = "Incorrect answer. Please enter again:";
+        boolean checker = false;
+        boolean answer = false;
+
+        do {
+            if (!scanner.hasNextBoolean()) {
+                System.out.println(errorMessage);
+                scanner.next();
+            } else {
+                answer = scanner.nextBoolean();
+                checker = true;
+            }
+        } while (!checker);
+
+        return answer;
+    }
+
+    private static void displayCart(){
+        System.out.println("Total fuel refilled:" + totalQuantity);
+        System.out.println("Total amount:" + totalAmount + CURRENCY);
+    }
+
+    private static int getMoney(){
+        return new Random().nextInt(2000 - 1) + 1;
     }
 }
+
 
 
 
